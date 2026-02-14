3a3eb2ce1aa018de4a1f5505a14db70d52e5d4ae
vinodgunaga <vbgunaga8@gmail.com>
2026-02-15 00:40:30 +0530

    api response, error handling and utils

--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path: "./env"
 })
+
+
 connectDB()
+.then(() => {
+
+    app.on('error', (err) => {
+        console.log("Error in express app ", err);
+        throw err;
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is Running at port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB Connection failed !!! ", err)
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+const asyncHandler = (requestHandler) => async (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next))
+    .catch((err) => {
+        next(err)
+    })
+}
+
+export {asyncHandler}
+
+
+
+// const asyncHandler = (func) => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+// const asyncHandler = (func) => async (req, res, next) => {
+//     try {
+
+//         await func(req, res, next)
+        
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+
+// }
