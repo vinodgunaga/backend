ec513dd98e8b674e490d0b8a282dce930d7ea693
vinodgunaga <vbgunaga8@gmail.com>
2026-03-08 18:49:05 +0530

    fix:user router wrong file name

--- src/app.js
+++ src/app.js
@@ -1,25 +1,27 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 //routes import
-import userRoter from "./routes/user.router.js"
+import userRoter from "./routes/user.routes.js";
 
 //routes declaration
-app.use("/api/v1/users", userRoter)
+app.use("/api/v1/users", userRoter);
 
 //http://localhost:8000/api/v1/users/register
 
-export { app }
+export { app };
